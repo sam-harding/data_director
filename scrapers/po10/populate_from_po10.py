0e13a12530084e57bd1fed302d894b5b86a72f68
Samuel Harding <samharding42@gmail.com>
2017-04-12 09:32:11 +0100

    Added populate_from_po10.py script to wrap scraper

--- scrapers/po10/populate_from_po10.py
+++ scrapers/po10/populate_from_po10.py
@@ -1,13 +1,17 @@
 import athlete
 import race
 import logging
+import json
 logging.basicConfig(filename='logs/po10populate.log',level=logging.DEBUG, format='[%(asctime)s][%(levelname)s]:%(message)s')
 
-a = athlete.scrape_athlete_po10(208016)
-logging.info("Processing Athlete {} ------ ".format(208016))
-for r in a["po10_races"]:
-  print("Attempting to process (meeting_id={}, event={}, venue={}, date={})".format(r["meeting_id"], r["event"], r["venue"], r["date"]))
-  print(race.scrape_race_po10(meeting_id=r["meeting_id"],
+with open("po10_sample.json", "w") as outfile:
+  a = athlete.scrape_athlete_po10(208016)
+  json.dump(a, outfile, indent=2, sort_keys=True)
+  logging.info("Processing Athlete {} ------ ".format(208016))
+  for r in a["po10_races"]:
+    print("Attempting to process (meeting_id={}, event={}, venue={}, date={})".format(r["meeting_id"], r["event"], r["venue"], r["date"]))
+    parsed = race.scrape_race_po10(meeting_id=r["meeting_id"],
                         event=r["event"],
                         venue=r["venue"],
-                        date=r["date"]))
+                        date=r["date"])
+    json.dump(parsed, outfile, indent=2, sort_keys=True)
